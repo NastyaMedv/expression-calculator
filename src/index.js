c59ebbf75e5c9573c575c572ed3eef9ea9273c4d
Anastasiya Medvedeva <33265018+NastyaMedv@users.noreply.github.com>
2019-10-12 16:26:10 +0300

    Update index.js
    simple calculations

--- src/index.js
+++ src/index.js
@@ -26,9 +26,77 @@ function expressionCalculator(expr) {    // write your solution here
   if (!check(expr))
     throw new SyntaxError('ExpressionError: Brackets must be paired');
 
+  function count(a,b,c) {             // counting function for all operations
+    switch (c) {
+      case '+': return (+a+b);
+      case '-': return (a-b);
+      case '*': return (a*b);
+      case '/': return (a/b);
+      default: return null; break;
+    }
+  }
 
+  function isFirst(str) {           // check for first operations * and /
+    return ((str=='*')||(str=='/')) ? true : false;
+  }
 
-  return 0;
+  function isSecond(str) {           // check for second operations + and -
+    return ((str=='+')||(str=='-')) ? true : false;
+  }
+
+  function findRight(str) {         // searching right number
+    let i = 0;
+    while ((!isFirst(str[i]))&&(!isSecond(str[i]))) {
+      i++;
+      if (i == str.length) break;
+    }
+    return i;
+  }
+
+  function findLeft(str) {        // searching left number
+    let i = str.length - 1;
+    while ((!isFirst(str[i]))&&(!isSecond(str[i]))) {
+      i--;
+      if (i == -1) break;
+    }
+    return str.length - i - 1;
+  }
+
+  function calculation(expr) {
+    let i = 0;
+
+    while (i<expr.length) {
+      if (isFirst(expr[i])) {
+        let numberA = findLeft(expr.substring(0,i));
+        let numberB = findRight(expr.substring(i+1));
+        let result = count(+expr.substr(i-numberA,numberA),+expr.substr(i+1,numberB),expr[i]);
+        expr = expr.substring(0,i-numberA) + result + expr.substring(i+numberB+1);
+        i-= numberA;
+//console.log(expr);
+      } else {
+        i++;
+      }
+    }
+
+    i = (expr[0]=='-') ? 1 : 0;
+
+    while (i<expr.length) {
+      if (isSecond(expr[i])) {
+        let numberA = expr.substring(0,i);
+        let numberB = findRight(expr.substring(i+1));
+        let result = count(numberA,+expr.substr(i+1,numberB),expr[i]);
+        expr = result + expr.substring(i+numberB+1);
+        i = (expr[0]=='-') ? 1 : 0;
+//console.log(expr);
+      } else {
+        i++;
+      }
+    }
+
+    return expr;
+  }
+
+  return +calculation(expr);
 
 }
 
